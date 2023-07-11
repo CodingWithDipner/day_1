ad233878c27222cd7663f29080a4fbd644695442
A Dipner Reddy <89031602+dipnerreddy@users.noreply.github.com>
2023-07-11 23:22:56 +0530

    Create main.java

---
+++ main.java
@@ -0,0 +1,19 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+
+
+/* Name of the class has to be "Main" only if the class is public. */
+public class main
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc=new Scanner(System.in); // Using Scanner class for taking input
+		
+		int input=sc.nextInt(); // taking input from the user
+		int output=input;
+		
+		System.out.println(output); // giving the output
+	}
+}
